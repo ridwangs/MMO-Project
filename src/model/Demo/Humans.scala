998039c21f29db691e3eba1bd558d7e3a96cc616
Jacky Zhu <jackyzhu@buffalo.edu>
2019-04-02 17:53:55 -0400

    model

---
+++ src/model/Demo/Humans.scala
@@ -0,0 +1,50 @@
+package model.Demo
+
+class Humans extends Animate_Objects (100, 100.0, 10, 5){
+  var inventory: List[Inanimate_Objects] = List()
+  var hungertime: Long = System.nanoTime()
+  override def consumeObject(consumedObject: Inanimate_Objects): Unit = {
+    if (consumedObject.canConsume) {
+      this.health = this.health + consumedObject.health
+      if (this.health > 100) {
+        this.health = 100
+      }
+    }
+  }
+
+  override def consumeFlesh(consumed: Inanimate_Objects): Unit = {
+
+  }
+
+  override def loseMonsterHP(attacker: Inanimate_Objects): Unit = {
+
+  }
+  override def loseHunger: Double = {
+    1/5
+  }
+  def hungerUpdate(time:Long):Unit = {
+    hunger = (time/1000000000 - hungertime/1000000000) * loseHunger
+    hungertime = time
+  }
+  override def loseHumanHP(attacker: Animate_Objects): Unit = {
+    health = health - attacker.strength
+  }
+
+  def pickUp(thing: Inanimate_Objects): Unit = {
+    if (thing.canPickUp) {
+      inventory = inventory :+ thing
+    }
+    if (inventory.length > 30) {
+      println("You're Over The Inventory Limit")
+    }
+  }
+  override def die: Boolean = {
+    if (this.health <= 0){
+      GameLobby.aliveHumans -= this
+      true
+    }
+    else{
+      false
+    }
+  }
+}
---
+++ src/model/Demo/Monster.scala
@@ -0,0 +1,49 @@
+package model.Demo
+
+class Monster extends Animate_Objects(50, 100, 15, 10){
+  var hungertime: Long = System.nanoTime()
+  override def consumeFlesh(consumed: Inanimate_Objects): Unit = {
+    if (consumed.health == 0){
+      this.health = this.health + 100
+    }
+  }
+
+  override def consumeObject(consumed: Inanimate_Objects): Unit = {
+
+  }
+
+  override def loseMonsterHP(attacker: Inanimate_Objects): Unit = {
+    health = health - attacker.damage
+  }
+
+  override def loseHunger: Double = {
+    2/5
+  }
+  def hungerLoss(time: Long): Unit = {
+    hunger = (time/1000000000 - hungertime/1000000000) * loseHunger
+    hungertime = System.nanoTime()
+  }
+  def enraged(): Unit = {
+    if(this.health <= 25){
+      speed = speed * 2
+      strength = strength * 1.5
+    }
+  }
+
+  override def die: Boolean = {
+    if (this.health <= 0){
+      GameLobby.aliveMonster -= this
+      true
+    }
+    else{
+      false
+    }
+  }
+
+  def consumeObject(consumed: Animate_Objects): Unit = {
+    health = health
+  }
+
+  override def loseHumanHP(attacker: Animate_Objects): Unit = {
+  }
+}
