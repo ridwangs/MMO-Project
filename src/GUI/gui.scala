badb6e13e7ce5b6046cb751c0f3b10dd63bfe08d
ridwangs <47649356+ridwangs@users.noreply.github.com>
2019-04-04 04:02:25 -0400

    good movement

--- src/GUI/gui.scala
+++ src/GUI/gui.scala
@@ -3,8 +3,7 @@ package GUI
 import model.Demo.fruits.{apple, banana, orange}
 
 import scala.util.Random
-import model.Demo.{Humans, Inanimate_Objects}
-import physics.collision
+import model.Demo.Humans
 //import javafx.scene.shape.Circle
 import scalafx.animation.AnimationTimer
 import scalafx.application.JFXApp
@@ -22,20 +21,18 @@ import scala.collection.immutable.Range
 
 
 object gui extends JFXApp {
-  var collision: collision = new collision
   var anyRandom = scala.util.Random
+  var allApple: List[apple] = List()
   val fruits: List[String] = List("apple", "banana", "orange")
 
   var allHumans: List[Humans] = List()
-  var allApple: List[apple] = List()
   var allBanana: List[banana] = List()
   var allOrange: List[orange] = List()
-  var allFruit: List[Inanimate_Objects] = List()
 
-  val maxWidth = 1920
-  val maxheight = 1080
+  val maxWidth = 1980
+  val maxheight = 1020
 
-  var timeSpawn = 4.0
+  var timeSpawn = 5.0
   var lastUpdateTime: Long = System.nanoTime()
   var objects = new Group {}
 
@@ -45,29 +42,6 @@ object gui extends JFXApp {
     objects.children.add(player.shape)
     allHumans = allHumans :+ player
   }
-  def collide(circle1: Circle, circle2: Circle): Boolean = {
-    var xdistance = circle1.centerX.toDouble - circle2.centerX.toDouble
-    var ydistance = circle1.centerY.toDouble - circle2.centerY.toDouble
-    var sumradius = Math.pow(xdistance,2) + Math.pow(ydistance,2)
-    if(sumradius < Math.pow(circle1.radius.toDouble + circle2.radius.toDouble,2)){
-      true
-    }
-    else{
-      false
-    }
-  }
-  def stayput(circle1: Circle, circle2: Circle): Unit = {
-    val sumradius = circle1.radius.toDouble + circle2.radius.toDouble
-    var xdistance = circle1.centerX.toDouble - circle2.centerX.toDouble
-    var ydistance = circle1.centerY.toDouble - circle2.centerY.toDouble
-    var length = Math.sqrt(Math.pow(xdistance, 2)+ Math.pow(ydistance, 2))
-    circle1.centerX = circle2.centerX.toDouble + sumradius * (xdistance/length)
-    circle1.centerY = circle2.centerY.toDouble + sumradius * (ydistance/length)
-  }
-  def consume(object1: Humans, object2: Inanimate_Objects, list: Group): Unit ={
-    object1.consumeObject(object2)
-    list.getChildren.remove(object2)
-  }
 
   def createFruits(x: String): Unit ={
     x match {
@@ -75,28 +49,22 @@ object gui extends JFXApp {
         val apple = new apple
         apple.shape.centerX = maxWidth*Math.random
         apple.shape.centerY = maxheight*Math.random
-        apple.shape.fill = Color.Red
         objects.children.add(apple.shape)
         allApple = allApple :+ apple
-        allFruit = allFruit :+ apple
       }
       case "orange" => {
         val orange = new orange
         orange.shape.centerX = maxWidth*Math.random
         orange.shape.centerY = maxheight*Math.random
-        orange.shape.fill = Color.Orange
         objects.children.add(orange.shape)
         allOrange = allOrange :+ orange
-        allFruit = allFruit :+ orange
       }
       case "banana" => {
         val banana = new banana
         banana.shape.centerX = maxWidth*Math.random
         banana.shape.centerY = maxheight*Math.random
-        banana.shape.fill = Color.Yellow
         objects.children.add(banana.shape)
         allBanana = allBanana :+ banana
-        allFruit = allFruit :+ banana
       }
       case _ =>
     }
@@ -107,10 +75,10 @@ object gui extends JFXApp {
 
  def keyPress(event: KeyCode): Unit = {
     event.getName match {
-      case "W" => h1.shape.translateY.value -= h1.speed*0.5
-      case "S" => h1.shape.translateY.value += h1.speed*0.5
-      case "A" => h1.shape.translateX.value -= h1.speed*0.5
-      case "D" => h1.shape.translateX.value += h1.speed*0.5
+      case "W" => h1.shape.translateY.value -= h1.speed*0.1
+      case "S" => h1.shape.translateY.value += h1.speed*0.1
+      case "A" => h1.shape.translateX.value -= h1.speed*0.1
+      case "D" => h1.shape.translateX.value += h1.speed*0.1
       case _ =>
     }
   }
@@ -133,12 +101,6 @@ object gui extends JFXApp {
           else {
             createFruits(fruits(anyRandom.nextInt(3)))
             timeSpawn = 5.0
-          }
-        }
-        for (things <- allFruit){
-          if(collide(allHumans.head.shape, things.shape) == true){
-//            consume(allHumans.head, things ,objects)
-            objects.children.remove(things)
           }
         }
       }
--- src/controller/KeyboardInputs.scala
+++ src/controller/KeyboardInputs.scala
@@ -2,10 +2,12 @@
 //
 //import javafx.event.EventHandler
 //import javafx.scene.input.KeyEvent
+//import model.Demo.Humans
 //
 //
 //
-//abstract class KeyboardInputs(player: Player) extends EventHandler[KeyEvent] {
+//
+//abstract class KeyboardInputs(player: Humans) extends EventHandler[KeyEvent] {
 //  val LEFT: String
 //  val RIGHT: String
 //  val UP: String
@@ -13,7 +15,6 @@
 //
 //  override def handle(event: KeyEvent): Unit = {
 //    val keyCode = event.getCode
-//
 //    event.getEventType.getName match {
 //      case "KEY_RELEASED" => keyCode.getName match {
 //        case this.LEFT => player.leftReleased()
@@ -36,7 +37,7 @@
 //  }
 //}
 //
-//abstract class MouseInputs() extends EventHandler[KeyEvent]{
+///*abstract class MouseInputs() extends EventHandler[KeyEvent]{
 //  override def handle(event: KeyEvent): Unit = {
 //    val keyCode = event.getCode
 //
@@ -45,19 +46,36 @@
 //    }
 //
 //  }
-//}
+//}*/
 //
 //
 //
-//class ArrowInputs(player: Player) extends KeyboardInputs(player) {
+//class ArrowInputs(player: Humans) extends KeyboardInputs(player) {
 //  override val LEFT: String = "Left"
 //  override val RIGHT: String = "Right"
 //  override val UP: String = "Up"
 //  override val DOWN: String = "Down"
 //
+//
+//  if(player.leftKeyHeld){
+//    player.shape.translateX.value -= player.speed*0.1
+//  }
+//
+//  if(player.rightKeyHeld){
+//    player.shape.translateX.value += player.speed*0.1
+//  }
+//
+//  if(player.upKeyHeld){
+//    player.shape.translateY.value -= player.speed*0.1
+//  }
+//
+//  if(player.downKeyHeld){
+//    player.shape.translateY.value += player.speed*0.1
+//  }
+//
 //}
 //
-//class WASDInputs(player: Player) extends KeyboardInputs(player) {
+//class WASDInputs(player: Humans) extends KeyboardInputs(player) {
 //  override val LEFT: String = "A"
 //  override val RIGHT: String = "D"
 //  override val UP: String = "W"
