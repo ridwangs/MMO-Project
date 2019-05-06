6ece4f726070093ce47d6605f94d2818b3a2e149
richard <rwu23@buffalo.edu>
2019-05-06 15:39:02 -0400

    began @socket.server.on

--- src/WebModel/server.py
+++ src/WebModel/server.py
@@ -14,7 +14,47 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 socket_server = SocketIO(app)
 
+scala_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+scala_socket.connect(('localhost', 8000))
 
+delimiter = "~"
+
+def listen_to_scala(the_socket):
+    buffer = ""
+    while True:
+        buffer += the_socket.recv(1024).decode()
+        while delimiter in buffer:
+            message = buffer[:buffer.find(delimiter)]
+            buffer = buffer[buffer.find(delimiter) + 1:]
+            get_from_scala(message)
+
+def get_from_scala(data):
+    socket_server.emit('gamestate', data, broadcast=True)
+
+def send_to_scala(data):
+    scala_socket.sendall((json.dumps(data) + delimiter).encode())
+
+
+Thread(target=listen_to_scala, args=(scala_socket,)).start()
+
+#sidToUsername = {}
+#health = {}
+#speed = {}
+#strength = {}
+
+@socket_server.on('connect')
+def got_message():
+    print(request.sid + " connected")
+    message = {"username": request.sid, "action": "connected"}
+    send_to_scala(message)
+
+
+
+@socket_server.on('disconnect')
+def disconnect():
+    print(request.sid + " closed")
+    message = {"username": request.sid, "action": "closed"}
+    send_to_scala(message)
 
 
 @app.route('/')
@@ -36,4 +76,5 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == "__main__":
-    socket_server.run(app, port=8080)
+    app.run(host= '0.0.0.0') #Not sure if this functions when connected with Scala
+    socket_server.run(app, port=60000)
