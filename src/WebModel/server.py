e1a3acf731c882b6fe7e251b99a05515b30b97b3
richard <rwu23@buffalo.edu>
2019-05-07 19:07:55 -0400

    updated

--- src/WebModel/server.py
+++ src/WebModel/server.py
@@ -13,49 +13,57 @@ eventlet.monkey_patch()
 
 app = Flask(__name__)
 socket_server = SocketIO(app)
-#
-# scala_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # scala_socket.connect(('localhost', 8000))
-#
-# delimiter = "~"
-#
-# def listen_to_scala(the_socket):
-#     buffer = ""
-#     while True:
-#         buffer += the_socket.recv(1024).decode()
-#         while delimiter in buffer:
-#             message = buffer[:buffer.find(delimiter)]
-#             buffer = buffer[buffer.find(delimiter) + 1:]
-#             get_from_scala(message)
-#
-# def get_from_scala(data):
-#     socket_server.emit('gamestate', data, broadcast=True)
-#
-# def send_to_scala(data):
-#     scala_socket.sendall((json.dumps(data) + delimiter).encode())
-#
-# #
-# # Thread(target=listen_to_scala, args=(scala_socket,)).start()
-#
-# #sidToUsername = {}
-# #health = {}
-# #speed = {}
-# #strength = {}
-#
+
+ scala_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ scala_socket.connect(('localhost', 8000))
+
+ delimiter = "~"
+
+ def listen_to_scala(the_socket):
+     buffer = ""
+     while True:
+         buffer += the_socket.recv(1024).decode()
+         while delimiter in buffer:
+             message = buffer[:buffer.find(delimiter)]
+             buffer = buffer[buffer.find(delimiter) + 1:]
+             get_from_scala(message)
+
+ def get_from_scala(data):
+     socket_server.emit('gamestate', data, broadcast=True)
+
+ def send_to_scala(data):
+     scala_socket.sendall((json.dumps(data) + delimiter).encode())
+
+
+Thread(target=listen_to_scala, args=(scala_socket,)).start()
+
+sidToUsername = {}
+usernametoSid = {}
+
+
+
 @socket_server.on('connect')
-def got_message():
-    print(request.sid + " connected")
-    message = {"username": request.sid, "action": "connected"}
-#    send_to_scala(message)
+def got_message(username):
+    sidToUsername[request.sid] = username
+    usernametoSid[username] = request.sid
+    print(username + " connected")
+    message = {"username": request.sid, "action": "spawn"}
 
-#
-#
-# @socket_server.on('disconnect')
-# def disconnect():
-#     print(request.sid + " closed")
-#     message = {"username": request.sid, "action": "closed"}
-#     send_to_scala(message)
 
+
+@socket_server.on('disconnect')
+def disconnect():
+    if request.sid in sidToUsername:
+        username = sidToUsername[request.sid]
+        del sidToUsername[request.sid]
+        del usernametoSid[username]
+        print(username + " disconnected")
+        message = {"username": username, "action": "disconnected"}
+
+@socket_server.on('keyStates')
+def key_state(jsonKeyStates):
+    print("moving")
+    message = {"username": request.sid, "action": "move", "key_states": jsonKeyStates}
 
 @app.route('/')
 def index():
@@ -76,5 +84,5 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == "__main__":
-  #app.run(host= '0.0.0.0') #Not sure if this functions when connected with Scala
+    #app.run(host= '0.0.0.0') #Not sure if this functions when connected with Scala
     socket_server.run(app, port=60000)
