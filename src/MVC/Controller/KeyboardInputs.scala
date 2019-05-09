c8a31d39841bcb9a14172c330d20987d5390b4c0
Jacky Zhu <jackyzhu@buffalo.edu>
2019-05-09 16:11:53 -0400

    working on keystate for gui

--- src/MVC/Controller/KeyboardInputs.scala
+++ src/MVC/Controller/KeyboardInputs.scala
@@ -1,45 +1,49 @@
-//package MVC.Controller
-//
-//import javafx.event.EventHandler
-//import javafx.scene.input.KeyEvent
-//import MVC.Model.Game
-//import MVC.Model.objects.Humans
-//
-//abstract class KeyboardInputs(playerKey: Game) extends EventHandler[KeyEvent] {
-//  val LEFT: String
-//  val RIGHT: String
-//  val UP: String
-//  val DOWN: String
-//  val SPACE: String
-//
-//  override def handle(event: KeyEvent): Unit = {
-//    val keyCode = event.getCode
-//
-//    event.getEventType.getName match {
-//      case "KEY_RELEASED" => keyCode.getName match {
-//        case this.LEFT => playerKey.h1.leftKeyHeld = false
-//        case this.RIGHT => playerKey.h1.rightKeyHeld = false
-//        case this.UP => playerKey.h1.upKeyHeld = false
-//        case this.DOWN => playerKey.h1.downKeyHeld = false
-//        case this.SPACE => playerKey.h1.spaceKeyHeld = false
-//        case _ =>
-//      }
-//
-//      case "KEY_PRESSED" => keyCode.getName match {
-//        case this.LEFT => playerKey.h1.leftKeyHeld = true
-//        case this.RIGHT => playerKey.h1.rightKeyHeld = true
-//        case this.UP => playerKey.h1.upKeyHeld = true
-//        case this.DOWN => playerKey.h1.downKeyHeld = true
-//        case this.SPACE => playerKey.h1.spaceKeyHeld = true
-//        case _ =>
-//      }
-//      case _ =>
-//
-//    }
-//  }
-//}
-//
-//class WASDInputs(player: Game) extends KeyboardInputs(player) {
+package MVC.Controller
+
+import javafx.event.EventHandler
+import javafx.scene.input.KeyEvent
+import MVC.Model.Game
+import MVC.Model.objects.Humans
+import io.socket.client.Socket
+
+class KeyboardInputs(socket: Socket) extends EventHandler[KeyEvent] {
+  var keys: Map[String, Boolean] = Map(
+    "leftKeyHeld" -> false,
+    "rightKeyHeld" -> false,
+    "upKeyHeld" -> false,
+    "downKeyHeld" -> false,
+    "spaceKeyHeld" -> false
+  )
+
+  override def handle(event: KeyEvent): Unit = {
+    val keyCode = event.getCode
+
+    event.getEventType.getName match {
+      case "KEY_RELEASED" => keyCode.getName match {
+        case "A" => keys("leftKeyHeld") = false
+        case "D" => keys("rightKeyHeld") = false
+        case "S" => keys("downKeyHeld") = false
+        case "W" => keys("upKeyHeld") = false
+        case "Space" => keys("spaceKeyHeld") = false
+        case _ =>
+      }
+
+      case "KEY_PRESSED" => keyCode.getName match {
+        case "A" => keys("leftKeyHeld") = true
+        case "D" => keys("rightKeyHeld") = true
+        case "S" => keys("downKeyHeld") = true
+        case "W" => keys("upKeyHeld") = true
+        case "Space" => keys("spaceKeyHeld") = true
+        case _ =>
+      }
+      case _ =>
+
+    }
+  }
+  socket.emit("keyStates", keys)
+}
+
+//class WASDInputs(player: Game, username: String) extends KeyboardInputs(player, username) {
 //  override val LEFT: String = "A"
 //  override val RIGHT: String = "D"
 //  override val UP: String = "W"
