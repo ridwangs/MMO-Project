56e8b144af22797be24b07a832fc184ac5395304
Jacky Zhu <jackyzhu@buffalo.edu>
2019-05-09 20:53:05 -0400

    key states implemented for gui

--- src/MVC/View/gui.scala
+++ src/MVC/View/gui.scala
@@ -12,22 +12,30 @@ import javafx.scene.input.KeyEvent
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Circle
 import play.api.libs.json.{JsValue, Json}
+import scalafx.animation.AnimationTimer
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 class HandleMessagesFromPython() extends Emitter.Listener {
+  var guiplayer: mutable.Map[String, Circle] = mutable.Map()
   override def call(objects: Object*): Unit = {
     Platform.runLater(()=> {
       val gameState: JsValue = Json.parse(objects.apply(0).toString)
-
       gui.fruitsList = (gameState \ "fruits").as[Map[String, Int]]
       gui.humans = (gameState \ "humans").as[List[Map[String, JsValue]]]
       gui.apples = (gameState \ "apples").as[List[Map[String, JsValue]]]
       gui.oranges = (gameState \ "oranges").as[List[Map[String, JsValue]]]
       gui.bananas = (gameState \ "bananas").as[List[Map[String, JsValue]]]
 
+
       for(a <- gui.humans) {
-        gui.createHuman(a("x").as[Double],a("y").as[Double])
+        if(guiplayer.contains(a("name").as[String])){
+          gui.moveHuman(a("x").as[Double], a("y").as[Double], guiplayer, a("name").as[String])
+        }
+          else{
+        gui.createHuman(a("x").as[Double],a("y").as[Double], guiplayer, a("name").as[String])
+      }
       }
 
       for((f,v) <- gui.fruitsList) {
@@ -66,17 +74,26 @@ object gui extends JFXApp {
   socket.connect()
   socket.emit("register", "gui1")
 
-
-  def createHuman(x: Double, y: Double): Unit ={
-    val player: Circle = new Circle {
+  def createHuman(x: Double, y: Double, guiplayer: mutable.Map[String, Circle], name: String): Unit ={
+    var player: Circle = new Circle {
       centerX = x
       centerY = y
       radius = hRad
       fill = Color.Blue
     }
+    guiplayer += (name -> player)
     sceneGraphics.children.add(player)
   }
-
+  def moveHuman(x: Double, y: Double, guiplayer: mutable.Map[String, Circle], name: String): Unit = {
+    sceneGraphics.children.remove(guiplayer(name))
+    var player: Circle = new Circle {
+      centerX = x
+      centerY = y
+      radius = hRad
+      fill = Color.Blue
+    }
+    createHuman(x, y, guiplayer, name)
+  }
   def createFruits(x: Double, y: Double, t: String): Unit ={
     val player: Circle = new Circle {
       centerX = x
@@ -86,6 +103,7 @@ object gui extends JFXApp {
       else if (t == "b") fill = Color.Yellow
       else if (t == "o") fill = Color.Orange
     }
+//    scene
     sceneGraphics.children.add(player)
   }
 
